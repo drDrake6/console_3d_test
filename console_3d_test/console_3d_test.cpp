68ebc0727e65fe41c2aba675e567dc3203b4df62
drDrake6 <drdrake1337@gmail.com>
2021-03-16 12:47:53 +0200

    add class_Diagram

--- console_3d_test/console_3d_test.cpp
+++ console_3d_test/console_3d_test.cpp
@@ -10,37 +10,37 @@ int main()
 	buffer->HideCursor(); 
 	buffer->NotHighLightConsole();
 	
-   Set<string> game_space;
-
-   game_space.Add("###################");
-   game_space.Add("#                 #");
-   game_space.Add("#                 #");
-   game_space.Add("#                 #");
-   game_space.Add("#                 #");
-   game_space.Add("#        ##       #");
-   game_space.Add("#        ##       #");
-   game_space.Add("#                 #");
-   game_space.Add("#                 #");
-   game_space.Add("#          ########");
-   game_space.Add("#                 #");
-   game_space.Add("#######      ######");
-   game_space.Add("#     #           #");
-   game_space.Add("#  ####           #");
-   game_space.Add("#                 #");
-   game_space.Add("#                 #");
-   game_space.Add("###################");
-
-   Map map(game_space);
-   Entity* player = new Player(2.0f, 12.0f, 0.0f, 60.0f, 1.5f, 47.0f, 1.5f, 30.0f, 0.25f, 2.0f, map);
-
-   FPS _fps;
-
-   while (true)
-   {
-	   _fps.current_Calc_Period();
-	   buffer->Render(map, player, _fps);
-	   if (!player->Controle(map, _fps)) break;
-   }
-
-   _getch();
+    Set<string> game_space;
+    
+    game_space.Add("###################");
+    game_space.Add("#                 #");
+    game_space.Add("#                 #");
+    game_space.Add("#                 #");
+    game_space.Add("#                 #");
+    game_space.Add("#        ##       #");
+    game_space.Add("#        ##       #");
+    game_space.Add("#                 #");
+    game_space.Add("#                 #");
+    game_space.Add("#          ########");
+    game_space.Add("#                 #");
+    game_space.Add("#######      ######");
+    game_space.Add("#     #           #");
+    game_space.Add("#  ####           #");
+    game_space.Add("#                 #");
+    game_space.Add("#                 #");
+    game_space.Add("###################");
+    
+    Map map(game_space);
+    Entity* player = new Player(2.0f, 12.0f, 0.0f, 60.0f, 1.5f, 47.0f, 1.5f, 30.0f, 0.25f, 2.0f, map);
+    
+    FPS _fps;
+    
+    while (true)
+    {
+	    _fps.current_Calc_Period();
+	    buffer->Render(map, player, _fps);
+	    if (!player->Controle(map, _fps)) break;
+    }
+    
+    _getch();
 }
